b16c2630c10d44a33444fd27561d9d1f821896ab
Hector Tobon <htobon@gmail.com>
2014-01-23 07:35:38 -0500

    Moviendo código de configuración
    Agregar las siguientes líneas al final del archivo de configuración:
    $options = array();
    $options["session.cookie_lifetime"] = 120;
    $options["session.gc_maxlifetime"] = 120;

--- config/smarty.php
+++ config/smarty.php
@@ -1,8 +1,7 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ Configuring Smarty, Session and Autoloader.
  */
 
 require_once dirname(__FILE__).'/'.'config.php';
@@ -15,5 +14,17 @@ $smarty->setCompileDir(WORKSPACE_DIR . '/templates_c');
 $smarty->setCacheDir(WORKSPACE_DIR . '/cache');
 $smarty->setConfigDir(WORKSPACE_DIR . '/config');
 
+// Session
+ini_set("session.cookie_lifetime",$options["session.cookie_lifetime"]);
+ini_set("session.gc_maxlifetime", $options["session.gc_maxlifetime"]);
+//error_reporting(E_ALL ^ E_DEPRECATED);
+
+// Autoloader 
+spl_autoload_register(function($class){
+    $class = str_replace('\\', '/', $class);
+    if (file_exists(WORKSPACE_DIR.'/'.$class.'.php')) {
+        require_once(WORKSPACE_DIR.'/'.$class.'.php');
+    }    
+});
 
 ?>
