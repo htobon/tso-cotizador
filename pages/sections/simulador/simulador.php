2bd1285bdf35de99e23ed06c28c0e4dcb85b21ad
hernandcb <carvajal.hernandavid@gmail.com>
2014-01-25 00:55:26 -0500

    Revisando la función sesionActiva

--- pages/sections/simulador/simulador.php
+++ pages/sections/simulador/simulador.php
@@ -6,6 +6,13 @@ require_once __DIR__."/../../../config/autoloader.php";
 use db\AccesoriosDB;
 use utils\Sesion;
 
+echo "-------".Sesion::sesionActiva();
+if(Sesion::sesionActiva()){
+  echo "654654654654";
+  //require_once __DIR__."/../../logout.php";
+  //return;
+}
+
 $accesorios = AccesoriosDB::getAccesorios();
 
 $smarty->assign("accesorios", $accesorios);
