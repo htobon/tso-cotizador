da77faf4c7231c54857b3ef8f471ccfa0a18689f
Sebastian Amariles Garcia <sebas932@gmail.com>
2014-02-05 10:32:01 -0500

    Arreglando Popup para seleccion de GPS`s
    Arreglando Popup para seleccion de GPS`s en cotizador en la seccion del
    cotizador con jquery mobile

--- js/cotizador.js
+++ js/cotizador.js
@@ -10,7 +10,7 @@ $(document).on('pageinit', function()
          * Si por el contrario, ningún GPS se ha seleccionado, entonces el
          * accesorio podrá ser seleccionado.
          */
-        alert($(event.target).attr("id"));
+        //alert($(event.target).attr("id"));
     });
 }
 );
