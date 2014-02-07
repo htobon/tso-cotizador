25a7962a97db0cda37bab45bec8fceff0a0740a7
Hernan <carvajal.hernandavid@gmail.com>
2014-02-07 01:11:55 -0500

    Agregado manejador restricciones gps-accesorios
    Se agrego el manejador de la tabla tso_gps_accesorios_restricciones y se
    mejoro la función actualizarAccesorio para que ya se agreguen las
    restricciones

--- classes/db/AccesoriosDB.php
+++ classes/db/AccesoriosDB.php
@@ -1,7 +1,9 @@
 <?php
 namespace db;
 require_once (__DIR__."/../../config/db.php");
+
 use stdClass;
+use db\AccesoriosGpsDB;
 
 /**
  * Esta clase se encarga de conectar a la base de datos y encontrar información de los accesorios.
@@ -73,6 +75,39 @@ class AccesoriosDB {
 
         return (count($accesorios) <= 0 ) ? null : $accesorios;
     }
+    
+    /**
+     * Busca todos los accesorios activos con el código dado.
+     * @return type
+     */
+    public static function getAccesorioActivoPorCodigo($codigo) {
+        $conn = getConn();
+        $sql = "SELECT * FROM tso_accesorios WHERE cod_accesorio = ? AND esta_activo = TRUE";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindValue(1, $codigo);
+        $stmt->execute();
+        $accesorioData = $stmt->fetchAll();
+
+        $accesorio = new stdClass();
+        $_accesorio = $accesorioData[0];
+
+        if(!empty($_accesorio)){
+            $accesorio->id = $_accesorio["id"];
+            $accesorio->nombre = $_accesorio["nombre"];
+            $accesorio->codAccesorio = $_accesorio["cod_accesorio"];
+            $accesorio->codInstalacion = $_accesorio["cod_instalacion"];
+            $accesorio->precioInstalacion = $_accesorio["precio_instalacion"];
+            $accesorio->precioAccesorio = $_accesorio["precio_accesorio"];
+            $accesorio->precioInstalacion = $_accesorio["precio_instalacion"];
+            $accesorio->descripcion = $_accesorio["descripcion"];
+            $accesorio->image = $_accesorio["image"];
+            $accesorio->posicionX = $_accesorio["posicion_x"];
+            $accesorio->posicionY = $_accesorio["posicion_y"];
+        }
+
+        return ( empty($accesorioData) ) ? null : $accesorio;
+    }
 
     /**
      * desactivarAccesorio Desactiva el accesorio indentificado con el 
@@ -108,7 +143,18 @@ class AccesoriosDB {
         // Desactivamos el accesorio actual
         $desactivado = self::desactivarAccesorio($accesorio->id);
 
-        return ($agregado && $desactivado);
+        // Obtenemos las restricciones con gps del accesorio actual
+        $restricciones = AccesoriosGpsDB::getRestriccionesPorAccesorio($accesorio->id);
+        // Obtenemos el id del regidtro insertado
+        $accesorioActualizado = self::getAccesorioActivoPorCodigo($accesorio->codAccesorio);
+
+        // Actualizamos las restricciones con el nuevo id y las agregamos a la bd
+        for($i=0; $i < count($restricciones); $i++){
+            $restricciones[$i]["accesorio_id"] = $accesorioActualizado ->id;
+        }
+        $restriccionesAgregadas = AccesoriosGpsDB::agregarRestricciones($restricciones);
+
+        return ($agregado && $desactivado && $restriccionesAgregadas);
     }
 
     /**
---
+++ classes/db/AccesoriosGpsDB.php
@@ -0,0 +1,97 @@
+<?php
+namespace db;
+require_once (__DIR__."/../../config/db.php");
+use stdClass;
+
+/**
+ * Esta clase se encarga de conectar a la base de datos y 
+ * manejar la información de la tabla tso_gps_accesorios_restricciones
+ *
+ * @author hdcarvajal
+ */
+
+class AccesoriosGpsDB {
+    /*
+     * Busca todos las restricciones entre accesorios y gps.
+     */
+    public static function getAccesoriosGpsRestricciones() {
+        $conn = getConn();
+        $sql = "SELECT * FROM tso_gps_accesorios_restricciones";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $restriccionesAG = $stmt->fetchAll();
+        
+        return $restriccionesAG;
+    }
+    
+    /**
+     * Obtiene las restricciones que relacionen el accesorio recibido
+     * @param  [int] $accesorioID identificador del accesorio
+     * @return [arrray]  arreglo con las restricciones o arreglo vacio
+     *                   si no hay ninguna restricción.
+     */
+    public static function getRestriccionesPorAccesorio($accesorioID){
+        $conn = getConn();
+        $sql = "SELECT * FROM tso_gps_accesorios_restricciones WHERE accesorio_id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(1, $accesorioID);
+        $stmt->execute();
+
+        $restriccionesAG = $stmt->fetchAll();
+
+        // Eliminamos el valor del id del arreglo
+        for ($i=0; $i < count($restriccionesAG); $i++) {
+            unset($restriccionesAG[$i]["id"]);
+        }
+        
+        return $restriccionesAG;   
+    }
+
+    /**
+     * Obtiene las restricciones que relacionen la unidad de gps recibida
+     * @param  [int] $gpsID identificador de la unidad gps
+     * @return [arrray]  arreglo con las restricciones o arreglo vacio
+     *                   si no hay ninguna restricción.
+     */
+    public static function getRestriccionesPorGPS($gpsID){
+        $conn = getConn();
+        $sql = "SELECT * FROM tso_gps_accesorios_restricciones WHERE unidad_gps_id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(1, $gpsID);
+        $stmt->execute();
+
+        $restriccionesAG = $stmt->fetchAll();
+        
+        return $restriccionesAG;   
+    }
+
+    /**
+     * Esta funcion agrega restricciones entre accesorios 
+     * y unidades gps a la base de date_offset_get()
+     * 
+     * @param  [array] $restricciones arreglo de arreglos asociativos de formato: 
+     *                 ("unidad_gps_id" => valor, "accesorio_id" => valor)
+     * @return [bool]  true si la restriccion se agrego correctamente, false si no
+     */
+    public static function agregarRestricciones($restricciones){
+        $conn = getConn();
+        $sql = "INSERT INTO tso_gps_accesorios_restricciones (unidad_gps_id, accesorio_id) VALUES ";
+        
+        $i = 0;
+        foreach ($restricciones as $restriccion) {
+            $sql .= ($i > 0) ? ", ( ?, ? )" : "( ?, ? )";
+            $i ++;
+        }
+
+
+        $stmt = $conn->prepare($sql);
+        for($i=0, $j=1; $i < count($restricciones); $i++, $j+=2){
+            $stmt->bindValue( ($j), $restricciones[$i]["unidad_gps_id"]);            
+            $stmt->bindValue( ($j+1), $restricciones[$i]["accesorio_id"]);            
+        }
+
+        $inserted_rows = $stmt->execute();
+        return ($inserted_rows > 0);
+    }
+}
