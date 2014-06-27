0e856d3d9667d41e352f9469e6c746d1aadd5763
Camilo <corozco@ecodev.com.co>
2014-06-26 21:10:09 -0500

    Se Agrega cuenta de gmail
    Se genera un cuenta de gmail, par aenvio de correos, queda configurada
    en el archivo conf.php

--- pages/sections/cotizador/sendEmail.php
+++ pages/sections/cotizador/sendEmail.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . "/../../../config/smarty.php";
+require_once __DIR__ . "/../../../config/config.php";
 require_once __DIR__ . "/../../../libs/PHPMailer/PHPMailerAutoload.php";
 
 class sendPdfEmail {
@@ -127,14 +128,16 @@ class sendPdfEmail {
     public function enviarEmail() {
 
         $mail = new PHPMailer;
+        
+        global $infoSmtp;
 
         $mail->isSMTP();                                      
-        $mail->Host = 'smtp.gmail.com';                   
-        $mail->SMTPAuth = true;                              
-        $mail->Username = '';          
-        $mail->Password = '';                     
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;                                
+        $mail->Host = $infoSmtp['host'];//'smtp.gmail.com';                   
+        $mail->SMTPAuth = $infoSmtp['SMTPAuth']; //true;                              
+        $mail->Username = $infoSmtp['Username']; //'tsomobile.cotizador@gmail.com';          
+        $mail->Password = $infoSmtp['Password']; //'tsocotizador';                     
+        $mail->SMTPSecure = $infoSmtp['SMTPSecure']; //'tls';
+        $mail->Port = $infoSmtp['Port']; //587;                                
         // ssl port 465
         // tsl port 587
 
